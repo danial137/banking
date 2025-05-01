9d725e9f2e8d66b4e40b49310f941f5160efe280
danial137 <danial79fakhrabadi@gmail.com>
2025-05-01 21:24:27 +0330

    feat: enhance AuthForm with validation and custom input components; add form handling with react-hook-form and zod

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -3,8 +3,42 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import CustomInput from './CustomInput'
+import { authFormSchema } from '@/lib/utils'
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null)
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof authFormSchema>>({
+        resolver: zodResolver(authFormSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof authFormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+
     return (
         <section className='auth-form'>
             <header className='flex flex-col gap-5 md:gap-8'>
@@ -26,8 +60,23 @@ const AuthForm = ({ type }: { type: string }) => {
             </header>
             {user ? (
                 <div className='flex flex-col gap-4 '>
-                    
+
                 </div>
+            ) : (
+                <>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+                            {/* for password field  */}
+
+                            <CustomInput control={form.control} name="username" label="username" placeholder="enter your user name " />
+                            <CustomInput control={form.control} name="password" label="password" placeholder="enter your password " />
+
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
+
+                </>
             )}
         </section>
     )
