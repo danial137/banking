a118bfca1818864a60f0425bf769a5b38f70a513
danial137 <danial79fakhrabadi@gmail.com>
2025-05-03 21:35:00 +0330

    refactor auth input

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,9 +24,11 @@ const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
 
+    const formSchema = authFormSchema(type)
+
     // 1. Define your form.
-    const form = useForm<z.infer<typeof authFormSchema>>({
-        resolver: zodResolver(authFormSchema),
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
         defaultValues: {
             email: "",
             password: ""
@@ -34,7 +36,7 @@ const AuthForm = ({ type }: { type: string }) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof authFormSchema>) {
+    function onSubmit(values: z.infer<typeof formSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         setIsLoading(true)
@@ -70,25 +72,47 @@ const AuthForm = ({ type }: { type: string }) => {
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            {type === "sign-up" && (
+                                <>
+                                        <div className='flex gap-4'>
+                                        <CustomInput control={form.control} name="firstName" label="First Name" placeholder="enter your first name " />
+                                        <CustomInput control={form.control} name="lastName" label="Last Name" placeholder="enter your last name " />
+                                    </div>
 
+                                    <CustomInput control={form.control} name="address1" label="Adress" placeholder="enter your adress " />
+                                    <CustomInput control={form.control} name="state" label="State" placeholder="ex:NY" />
+                                    <CustomInput control={form.control} name="postalCode" label="Postal Code" placeholder="ex:11101 " />
+                                    <CustomInput control={form.control} name="dateOfBirth" label="Date of Birth" placeholder="yyy-mm-dd " />
+                                    <CustomInput control={form.control} name="ssn" label="SNN" placeholder="ex:1234" />
+
+                                </>
+                            )}
                             {/* for password field  */}
 
                             <CustomInput control={form.control} name="email" label="Email" placeholder="enter your user name " />
                             <CustomInput control={form.control} name="password" label="Password" placeholder="enter your password " />
 
-                            <Button type="submit" className='form-btn' disabled={isLoading}>
+                            <div className='flex flex-col gap-4'>
+                                <Button type="submit" className='form-btn' disabled={isLoading}>
 
-                                {isLoading ? (
+                                    {isLoading ? (
 
-                                    <>
-                                        <Loader2 size={20}
-                                            className='animate-spin' />&nbsp;
-                                        Loading...
-                                    </>
+                                        <>
+                                            <Loader2 size={20}
+                                                className='animate-spin' />&nbsp;
+                                            Loading...
+                                        </>
 
-                                ) : type === 'Sign In' ? 'Sign in' : 'sign up'}
+                                    ) : type === 'Sign In' ? 'Sign in' : 'sign up'}
 
-                            </Button>
+                                </Button>
+
+
+                            </div>
+
+
+
+
                         </form>
                     </Form>
 
@@ -100,7 +124,7 @@ const AuthForm = ({ type }: { type: string }) => {
                             {type === 'sign-in' ? "Don't have an account?" : "Already have an account ?"}
                         </p>
 
-                        <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-links'>
+                        <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
                             {type === 'sign-in' ? 'sign up' : 'Sign in'}
                         </Link>
 
