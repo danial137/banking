d10283500fedd0bd36d1516561ff4784d4135f9f
danial137 <danial79fakhrabadi@gmail.com>
2025-05-08 21:37:46 +0330

    feat: integrate Sentry for error monitoring and performance tracking
    - Added Sentry SDK to package.json for Next.js integration.
    - Created a faulty API route to test Sentry's error monitoring.
    - Configured Sentry initialization for client-side instrumentation.
    - Set up Sentry for server-side and edge runtime configurations.
    - Implemented error capturing for requests and router transitions.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0163d55b7f3c04ad947dc8ced79ec136@o4509203584385024.ingest.us.sentry.io/4509288061403136",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
